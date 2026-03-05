b2d6d2c8fc51bfb1119abb4cadcdc0525b598a5e
tusharmaurya2k <89917676+tusharmaurya2k@users.noreply.github.com>
2026-03-06 02:12:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
-//your code here!
+const list = document.getElementById("infi-list");
 
+let count = 1;
+
+// function to add list items
+function addItems(num) {
+  for (let i = 0; i < num; i++) {
+    const li = document.createElement("li");
+    li.textContent = "Item " + count++;
+    list.appendChild(li);
+  }
+}
+
+// add 10 items initially
+addItems(10);
+
+// detect scroll
+list.addEventListener("scroll", () => {
+  if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
+    addItems(2);
+  }
+});
